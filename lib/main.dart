d381e0ec8799900fb53aa9e50d427a03ae3fc1b1
Akhil Gupta <akhilgupta18ald@gmail.com>
2024-12-02 17:46:31 +0530

    Added multiple screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/categories.dart';
+import 'package:meals/screeens/tabs.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -19,7 +19,7 @@ class App extends StatelessWidget {
   const App({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: theme, home: const CategoriesScreen() //Todo
+    return MaterialApp(theme: theme, home: TabsScreen() //Todo
         );
   }
 }
